f6d9bf61a0bb88f4f0243a331d30bc6834d3fcd5
vichedanarith <vichedanarith2027@u.northwestern.edu>
2024-12-25 12:26:49 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { NavBar } from "./components/NavBar";
 import { Tools } from "./routes/Tools";
 import { Tutorial } from "./routes/Tutorial";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
